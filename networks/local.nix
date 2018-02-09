0ade3b0bb268d0122e4e756c8858fdb176461e03
Michael Bock <me@thought2.de>
2018-02-09 21:45:45 +0100

    Initial work

---
+++ networks/local.nix
@@ -0,0 +1,15 @@
+{
+  network.description = "Web server";
+
+  webserver =
+    { config, pkgs, ... }:
+    { services.httpd.enable = true;
+      services.httpd.adminAddr = "alice@example.org";
+      services.httpd.documentRoot = "${pkgs.valgrind.doc}/share/doc/valgrind/html";
+      networking.firewall.allowedTCPPorts = [ 80 ];
+
+      deployment.targetEnv = "virtualbox";
+      deployment.virtualbox.memorySize = 1024; # megabytes
+      deployment.virtualbox.vcpu = 2; # number of cpus
+    };
+}
